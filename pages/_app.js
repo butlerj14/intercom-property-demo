dad528d06e234589e389a6659a0d6ac131cd1624
James Butler <james.butler14@outlook.com>
2025-05-27 21:34:46 +0100

    url attribute

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.Intercom) {
+        window.Intercom('update', {
+          last_visited_url: url,
+        });
+      }
+    };
+
+    // Run on first load
+    handleRouteChange(window.location.pathname);
+
+    // Listen for future route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
