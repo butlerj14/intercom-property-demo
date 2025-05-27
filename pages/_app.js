145c8d27de4f2b8fcbb82db2093f5237da7f1f6a
James Butler <james.butler14@outlook.com>
2025-05-27 22:09:24 +0100

    Fix: Intercom widget ID to match app_id 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,27 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import Script from 'next/script';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    const intercomSettings = {
-      app_id: 't0dl0ok5',
-      email: 'test@saasypeople.com', // Replace with dynamic data if needed
-      name: 'Test User'              // Replace with dynamic data if needed
-    };
-
-    // Wait until Intercom is loaded
-    const bootIntercom = () => {
-      if (typeof window.Intercom === 'function') {
-        window.Intercom('boot', intercomSettings);
-      } else {
-        // If Intercom hasn't loaded yet, try again shortly
-        setTimeout(bootIntercom, 500);
-      }
-    };
-
-    bootIntercom();
-
     const handleRouteChange = (url) => {
-      if (typeof window.Intercom === 'function') {
+      if (window.Intercom) {
         window.Intercom('update', {
           last_visited_url: url,
         });
       }
     };
 
+    handleRouteChange(window.location.href); // initial load
     router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, []);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
@@ -45,25 +31,7 @@ export default function App({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
             window.intercomSettings = { app_id: "t0dl0ok5" };
-            (function(){
-              var w=window;var ic=w.Intercom;
-              if(typeof ic==="function"){
-                ic('reattach_activator'); ic('update', intercomSettings);
-              } else {
-                var d=document; var i=function(){i.c(arguments)}; i.q=[];
-                i.c=function(args){i.q.push(args)}; w.Intercom=i;
-                function l(){
-                  var s=d.createElement('script');
-                  s.type='text/javascript'; s.async=true;
-                  s.src='https://widget.intercom.io/widget/t0dl0ok5';
-                  var x=d.getElementsByTagName('script')[0];
-                  x.parentNode.insertBefore(s,x);
-                }
-                if(document.readyState==='complete'){l();}
-                else if(w.attachEvent){w.attachEvent('onload',l);}
-                else {w.addEventListener('load',l,false);}
-              }
-            })()
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/t0dl0ok5';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
           `,
         }}
       />
