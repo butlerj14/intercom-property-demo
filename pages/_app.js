27351ef6d9821ded4e3ba2612327c98a91cfdb48
James Butler <james.butler14@outlook.com>
2025-05-27 21:18:50 +0100

    app change

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.intercomSettings = { app_id: "sbkmi1lj" };
+            window.intercomSettings = { app_id: "t0dl0ok5" };
             (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/sbkmi1lj';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
           `,
         }}
