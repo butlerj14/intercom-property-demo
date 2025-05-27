f58c131fed7a477ba2cf4ca924cbd936a92098ea
James Butler <james.butler14@outlook.com>
2025-05-27 21:58:43 +0100

    Fix: Intercom widget ID to match app_id

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,18 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import Script from 'next/script';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
-  setTimeout(() => {
     if (typeof window.Intercom === 'function') {
-      window.Intercom('update', { last_visited_url: url });
-    } else {
-      console.warn('Intercom not ready yet.');
+      window.Intercom('update', {
+        last_visited_url: window.location.href,
+      });
     }
-  }, 500); // wait 0.5s to ensure Intercom is loaded
-};
-
-
-    // Run on first load
-    handleRouteChange(window.location.pathname);
-
-    // Listen for future route changes
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+  }, [router.asPath]);
 
   return (
     <>
@@ -36,7 +22,25 @@ export default function App({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
             window.intercomSettings = { app_id: "t0dl0ok5" };
-            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/t0dl0ok5';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+            (function(){
+              var w=window;var ic=w.Intercom;
+              if(typeof ic==="function"){
+                ic('reattach_activator'); ic('update', intercomSettings);
+              } else {
+                var d=document; var i=function(){i.c(arguments)}; i.q=[];
+                i.c=function(args){i.q.push(args)}; w.Intercom=i;
+                function l(){
+                  var s=d.createElement('script');
+                  s.type='text/javascript'; s.async=true;
+                  s.src='https://widget.intercom.io/widget/t0dl0ok5';
+                  var x=d.getElementsByTagName('script')[0];
+                  x.parentNode.insertBefore(s,x);
+                }
+                if(document.readyState==='complete'){l();}
+                else if(w.attachEvent){w.attachEvent('onload',l);}
+                else {w.addEventListener('load',l,false);}
+              }
+            })()
           `,
         }}
       />
