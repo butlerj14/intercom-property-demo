351000d9eeb93284cf2426740fa781d4a465f5df
James Butler <james.butler14@outlook.com>
2025-05-27 21:43:49 +0100

    widget id fix

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,15 @@ export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      if (window.Intercom) {
-        window.Intercom('update', {
-          last_visited_url: url,
-        });
-      }
-    };
+  setTimeout(() => {
+    if (typeof window.Intercom === 'function') {
+      window.Intercom('update', { last_visited_url: url });
+    } else {
+      console.warn('Intercom not ready yet.');
+    }
+  }, 500); // wait 0.5s to ensure Intercom is loaded
+};
+
 
     // Run on first load
     handleRouteChange(window.location.pathname);
@@ -33,7 +36,7 @@ export default function App({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
             window.intercomSettings = { app_id: "t0dl0ok5" };
-            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/sbkmi1lj';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/t0dl0ok5';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
           `,
         }}
       />
