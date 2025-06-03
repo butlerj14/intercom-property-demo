17f10d4c3951e47150e060e38badd5a6e27364c5
James Butler <james.butler14@outlook.com>
2025-06-03 18:13:48 +0100

    fixed verification

--- pages/_app.js
+++ pages/_app.js
@@ -30,43 +30,44 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.intercomSettings = {
-              app_id: "t0dl0ok5",
-              email: "test@example.com"
-              last_visited_url: window.location.href
-            };
+    window.intercomSettings = {
+      app_id: "t0dl0ok5",
+      user_id: "user_123456",
+      last_visited_url: window.location.href
+    };
 
-            (function(){
-              var w = window;
-              var ic = w.Intercom;
-              if (typeof ic === "function") {
-                ic('reattach_activator');
-                ic('update', w.intercomSettings);
-              } else {
-                var d = document;
-                var i = function() { i.c(arguments); };
-                i.q = [];
-                i.c = function(args) { i.q.push(args); };
-                w.Intercom = i;
-                var l = function() {
-                  var s = d.createElement('script');
-                  s.type = 'text/javascript';
-                  s.async = true;
-                  s.src = 'https://widget.intercom.io/widget/t0dl0ok5';
-                  var x = d.getElementsByTagName('script')[0];
-                  x.parentNode.insertBefore(s, x);
-                };
-                if (document.readyState === 'complete') {
-                  l();
-                } else if (w.attachEvent) {
-                  w.attachEvent('onload', l);
-                } else {
-                  w.addEventListener('load', l, false);
-                }
-              }
-            })();
-          `,
+    (function(){
+      var w = window;
+      var ic = w.Intercom;
+      if (typeof ic === "function") {
+        ic('reattach_activator');
+        ic('update', w.intercomSettings);
+      } else {
+        var d = document;
+        var i = function() { i.c(arguments); };
+        i.q = [];
+        i.c = function(args) { i.q.push(args); };
+        w.Intercom = i;
+        var l = function() {
+          var s = d.createElement('script');
+          s.type = 'text/javascript';
+          s.async = true;
+          s.src = 'https://widget.intercom.io/widget/t0dl0ok5';
+          var x = d.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(s, x);
+        };
+        if (document.readyState === 'complete') {
+          l();
+        } else if (w.attachEvent) {
+          w.attachEvent('onload', l);
+        } else {
+          w.addEventListener('load', l, false);
+        }
+      }
+    })();
+  `,
         }}
+
       />
       <Component {...pageProps} />
     </>
