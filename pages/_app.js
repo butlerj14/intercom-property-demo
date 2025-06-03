48380833dfbce2443284a441d876096d66c0be70
James Butler <james.butler14@outlook.com>
2025-06-03 18:37:27 +0100

    boot to intercom

--- pages/_app.js
+++ pages/_app.js
@@ -31,40 +31,40 @@ export default function App({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
     window.intercomSettings = {
-      app_id: "t0dl0ok5",
-      user_id: "user_123456",
-      last_visited_url: window.location.href
-    };
+  app_id: "t0dl0ok5",
+  user_id: "user_123456",
+  last_visited_url: window.location.href
+};
 
-    (function(){
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === "function") {
-        ic('reattach_activator');
-        ic('boot', w.intercomSettings);
-      } else {
-        var d = document;
-        var i = function() { i.c(arguments); };
-        i.q = [];
-        i.c = function(args) { i.q.push(args); };
-        w.Intercom = i;
-        var l = function() {
-          var s = d.createElement('script');
-          s.type = 'text/javascript';
-          s.async = true;
-          s.src = 'https://widget.intercom.io/widget/t0dl0ok5';
-          var x = d.getElementsByTagName('script')[0];
-          x.parentNode.insertBefore(s, x);
-        };
-        if (document.readyState === 'complete') {
-          l();
-        } else if (w.attachEvent) {
-          w.attachEvent('onload', l);
-        } else {
-          w.addEventListener('load', l, false);
-        }
-      }
-    })();
+(function(){
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic('shutdown'); // prevent reboots in dev
+    ic('boot', w.intercomSettings); // ✅ this line boots Intercom with user info
+  } else {
+    var d = document;
+    var i = function() { i.c(arguments); };
+    i.q = [];
+    i.c = function(args) { i.q.push(args); };
+    w.Intercom = i;
+    var l = function() {
+      var s = d.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://widget.intercom.io/widget/t0dl0ok5';
+      var x = d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    if (document.readyState === 'complete') {
+      l();
+    } else if (w.attachEvent) {
+      w.attachEvent('onload', l);
+    } else {
+      w.addEventListener('load', l, false);
+    }
+  }
+})();
   `,
         }}
       />
